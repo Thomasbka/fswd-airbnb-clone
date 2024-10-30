92d507def6d0f1601da0bef320eb97705cffa31b
Thomas <163157813+Thomasbka@users.noreply.github.com>
2024-10-30 11:38:26 +0100

    Log in and Sign up components created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :sessions
   has_many :properties
-  has many :bookings
+  has_many :bookings
 
   validates :username, presence: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
